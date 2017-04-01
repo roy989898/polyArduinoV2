40784a4ad5fd06238d75245fd2e2fc4ead331219
poming\pomingpo <roy989898@gmail.com>
2017-04-02 00:26:17 +0800

    autoformat

--- sketch_apr02a/sketch_apr02a.ino
+++ sketch_apr02a/sketch_apr02a.ino
@@ -1,14 +1,14 @@
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
-  pinMode(13,OUTPUT);
-
+  pinMode(13, OUTPUT);
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
-  digitalWrite(13,HIGH);
+  digitalWrite(13, HIGH);
   delay(1000);
-  digitalWrite(13,LOW);
+  digitalWrite(13, LOW);
   delay(1000);
-
 }
