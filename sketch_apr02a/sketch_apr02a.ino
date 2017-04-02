d7ffb2169acc730ffbc5c044547093ff66644025
poming\pomingpo <roy989898@gmail.com>
2017-04-02 12:49:13 +0800

    add utSensor linrary

--- sketch_apr02a/sketch_apr02a.ino
+++ sketch_apr02a/sketch_apr02a.ino
@@ -1,10 +1,16 @@
 #include "motor.h"
+#include "utSensor.h"
 
+int trig=2;
+int echo=3;
 void setup()
 {
   // put your setup code here, to run once:
   // pinMode(13, OUTPUT);
   setupMotor();
+  pinMode(trig, OUTPUT);
+  pinMode(echo, INPUT);
+  Serial.begin(9600);  
 }
 
 void loop()
@@ -15,10 +21,17 @@ void loop()
   // digitalWrite(13, LOW);
   // delay(1000);
 
-  rightMotorMoveBack(200);
-  leftMotorMoveFont(200);
-  delay(2000);
-  rightMotorMoveBack(0);
-  leftMotorMoveFont(0);
-  delay(2000);
+// test motor code
+  // rightMotorMoveBack(200);
+  // leftMotorMoveFont(200);
+  // delay(2000);
+  // rightMotorMoveBack(0);
+  // leftMotorMoveFont(0);
+  // delay(2000);
+
+  // test ultra sensor code
+  long int distance=detectDistance(trig,echo);
+  Serial.print(distance);
+  Serial.println(" cm");
+  delay(1000);   
 }
---
+++ sketch_apr02a/utSensor.h
@@ -0,0 +1,38 @@
+
+
+// need set up
+
+
+// pinMode(trig, OUTPUT);
+// pinMode(echo, INPUT);
+#include "Arduino.h"
+
+
+
+void Trig(int trig) {
+digitalWrite(trig, 0);
+delayMicroseconds(2);
+digitalWrite(trig, 1);
+delayMicroseconds(10);
+digitalWrite(trig, 0);
+}
+
+long int hearEcho(int echo) {
+return pulseIn(echo, HIGH);
+}
+
+
+long int microToCm(long int micro) {
+return micro*304*100/2/1000000;
+}
+
+
+long int detectDistance(int trig,int echo) {
+Trig(trig);
+long int  duration = hearEcho(echo);
+long int  distance = microToCm(duration);
+
+return distance;
+
+
+}
