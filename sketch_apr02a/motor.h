e328999c33cb243be99034775c31a937d33d6f2b
poming\pomingpo <roy989898@gmail.com>
2017-04-02 12:16:03 +0800

    add motor library

---
+++ sketch_apr02a/motor.h
@@ -0,0 +1,82 @@
+#include "Arduino.h"
+
+
+int inPMW1=5;
+int in2=12;
+int in3PMW=6;
+int in4=13;
+
+// pinMode(int1,OUTPUT);
+// pinMode(int2,OUTPUT);
+// pinMode(in3,OUTPUT);
+// pinMode(in4,OUTPUT);
+
+
+// when int3>in4,move back,right
+void setupMotor(){
+  pinMode(inPMW1,OUTPUT);
+  pinMode(in2,OUTPUT);
+  pinMode(in3PMW,OUTPUT);
+  pinMode(in4,OUTPUT);
+}
+
+void leftMotorMove(int outPmw3,int out4){
+  analogWrite(in3PMW, outPmw3);
+  digitalWrite(in4, out4);
+
+
+}
+
+void leftMotorMoveBack(int pmw){
+  leftMotorMove(pmw,0);
+
+}
+
+void leftMotorMoveFont(int pmw){
+  leftMotorMove(255-pmw,1);
+
+}
+
+void rightMotorMove(int outPmw1,int out2){
+  analogWrite(inPMW1, outPmw1);
+  digitalWrite(in2, out2);
+
+
+}
+
+void rightMotorMoveBack(int pmw){
+  rightMotorMove(pmw,0);
+}
+
+void rightMotorMoveFont(int pmw){
+  rightMotorMove(255-pmw,1);
+}
+
+
+void mototMoveFont(int pmw){
+  rightMotorMoveFont(pmw);
+  leftMotorMoveFont(pmw);
+}
+
+void motorMoveBack(int pmw){
+  rightMotorMoveBack(pmw);
+  leftMotorMoveBack(pmw);
+
+}
+
+void motorMoveLeft(int pmw){
+  rightMotorMoveFont(pmw);
+  leftMotorMoveBack(0);
+
+}
+
+void motorMoveRight(int pmw){
+  leftMotorMoveFont(pmw);
+  rightMotorMoveFont(0);
+
+}
+
+void motorStop(){
+  leftMotorMoveBack(0);
+  rightMotorMoveFont(0);
+}
--- sketch_apr02a/sketch_apr02a.ino
+++ sketch_apr02a/sketch_apr02a.ino
@@ -1,14 +1,24 @@
+#include "motor.h"
+
 void setup()
 {
   // put your setup code here, to run once:
-  pinMode(13, OUTPUT);
+  // pinMode(13, OUTPUT);
+  setupMotor();
 }
 
 void loop()
 {
   // put your main code here, to run repeatedly:
-  digitalWrite(13, HIGH);
-  delay(1000);
-  digitalWrite(13, LOW);
-  delay(1000);
+  // digitalWrite(13, HIGH);
+  // delay(1000);
+  // digitalWrite(13, LOW);
+  // delay(1000);
+
+  rightMotorMoveBack(200);
+  leftMotorMoveFont(200);
+  delay(2000);
+  rightMotorMoveBack(0);
+  leftMotorMoveFont(0);
+  delay(2000);
 }
