ab4ba473e229c7f1f44b43f73d80d62dcb80b4cc
poming\pomingpo <roy989898@gmail.com>
2017-04-02 15:07:21 +0800

    auto format2

--- sketch_apr02a/motor.h
+++ sketch_apr02a/motor.h
@@ -1,82 +1,82 @@
 #include "Arduino.h"
 
-
-int inPMW1=5;
-int in2=12;
-int in3PMW=6;
-int in4=13;
+int inPMW1 = 5;
+int in2 = 12;
+int in3PMW = 6;
+int in4 = 13;
 
 // pinMode(int1,OUTPUT);
 // pinMode(int2,OUTPUT);
 // pinMode(in3,OUTPUT);
 // pinMode(in4,OUTPUT);
 
-
 // when int3>in4,move back,right
-void setupMotor(){
-  pinMode(inPMW1,OUTPUT);
-  pinMode(in2,OUTPUT);
-  pinMode(in3PMW,OUTPUT);
-  pinMode(in4,OUTPUT);
+void setupMotor()
+{
+  pinMode(inPMW1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in3PMW, OUTPUT);
+  pinMode(in4, OUTPUT);
 }
 
-void leftMotorMove(int outPmw3,int out4){
+void leftMotorMove(int outPmw3, int out4)
+{
   analogWrite(in3PMW, outPmw3);
   digitalWrite(in4, out4);
-
-
 }
 
-void leftMotorMoveBack(int pmw){
-  leftMotorMove(pmw,0);
-
+void leftMotorMoveBack(int pmw)
+{
+  leftMotorMove(pmw, 0);
 }
 
-void leftMotorMoveFont(int pmw){
-  leftMotorMove(255-pmw,1);
-
+void leftMotorMoveFont(int pmw)
+{
+  leftMotorMove(255 - pmw, 1);
 }
 
-void rightMotorMove(int outPmw1,int out2){
+void rightMotorMove(int outPmw1, int out2)
+{
   analogWrite(inPMW1, outPmw1);
   digitalWrite(in2, out2);
-
-
 }
 
-void rightMotorMoveBack(int pmw){
-  rightMotorMove(pmw,0);
+void rightMotorMoveBack(int pmw)
+{
+  rightMotorMove(pmw, 0);
 }
 
-void rightMotorMoveFont(int pmw){
-  rightMotorMove(255-pmw,1);
+void rightMotorMoveFont(int pmw)
+{
+  rightMotorMove(255 - pmw, 1);
 }
 
-
-void mototMoveFont(int pmw){
+void mototMoveFont(int pmw)
+{
   rightMotorMoveFont(pmw);
   leftMotorMoveFont(pmw);
 }
 
-void motorMoveBack(int pmw){
+void motorMoveBack(int pmw)
+{
   rightMotorMoveBack(pmw);
   leftMotorMoveBack(pmw);
-
 }
 
-void motorMoveLeft(int pmw){
+void motorMoveLeft(int pmw)
+{
   rightMotorMoveFont(pmw);
   leftMotorMoveBack(0);
-
 }
 
-void motorMoveRight(int pmw){
+void motorMoveRight(int pmw)
+{
   leftMotorMoveFont(pmw);
   rightMotorMoveFont(0);
-
 }
 
-void motorStop(){
+void motorStop()
+{
   leftMotorMoveBack(0);
   rightMotorMoveFont(0);
 }
--- sketch_apr02a/sketch_apr02a.ino
+++ sketch_apr02a/sketch_apr02a.ino
@@ -1,12 +1,12 @@
 #include "motor.h"
 #include "utSensor.h"
 
-int Rtrig=2;
-int Recho=3;
-int Ctrig=8;
-int Cecho=9;
-int Ltrig=7;
-int Lecho=10;
+int Rtrig = 2;
+int Recho = 3;
+int Ctrig = 8;
+int Cecho = 9;
+int Ltrig = 7;
+int Lecho = 10;
 void setup()
 {
   // put your setup code here, to run once:
@@ -14,11 +14,11 @@ void setup()
   setupMotor();
   pinMode(Rtrig, OUTPUT);
   pinMode(Recho, INPUT);
-   pinMode(Ctrig, OUTPUT);
+  pinMode(Ctrig, OUTPUT);
   pinMode(Cecho, INPUT);
   pinMode(Ltrig, OUTPUT);
   pinMode(Lecho, INPUT);
-  Serial.begin(9600);  
+  Serial.begin(9600);
 }
 
 void loop()
@@ -29,7 +29,7 @@ void loop()
   // digitalWrite(13, LOW);
   // delay(1000);
 
-// test motor code
+  // test motor code
   // rightMotorMoveBack(200);
   // leftMotorMoveFont(200);
   // delay(2000);
@@ -38,22 +38,18 @@ void loop()
   // delay(2000);
 
   // test ultra sensor code
-  long int Rdistance=detectDistance(Rtrig,Recho);
-  // Serial.print("right ");
-  // Serial.print(Rdistance);
-  // Serial.println(" cm");
+  long int Rdistance = detectDistance(Rtrig, Recho);
+  /* Serial.print("right ");
+  Serial.print(Rdistance);
+  Serial.println(" cm");*/
 
-  long int Cdistance=detectDistance(Ctrig,Cecho);
-  // Serial.print("center ");
-  // Serial.print(Cdistance);
-  // Serial.println(" cm");
-  
+  long int Cdistance = detectDistance(Ctrig, Cecho);
+  /*  Serial.print("center ");
+  Serial.print(Cdistance);
+  Serial.println(" cm");*/
 
-  long int Ldistance=detectDistance(Ltrig,Lecho);
-  // Serial.print("left ");
-  // Serial.print(Ldistance);
-  // Serial.println(" cm");
-
-
-
+  long int Ldistance = detectDistance(Ltrig, Lecho);
+  /* Serial.print("left ");
+  Serial.print(Ldistance);
+  Serial.println(" cm");*/
 }
